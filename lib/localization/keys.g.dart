6c30d1d81c304fe544cd897754b5f9fd569e0ef3
Oleh Balychev <oleh.balychev@extrawest.com>
2021-10-19 20:37:02 +0300

    added app structure

---
+++ lib/localization/keys.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'keys.dart';
+
+// **************************************************************************
+// Generator: FlutterTranslateGen
+// **************************************************************************
+
+class Keys {
+  static const String Test_Page_Title = 'test_page.title';
+}
